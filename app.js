64c06193839e7126ae559346c1f832d46c8844c6
AliShakiba <prance5731.mania@gmail.com>
2022-08-22 11:21:55 +0430

    v.2.1

--- app.js
+++ app.js
@@ -14,8 +14,8 @@ form.addEventListener("submit", (e) => {
     .then((response) => {
       console.log(response.data);
       const { weather, main, sys, name } = response.data;
-      const icon = `https://s3-us-west-2.amazonaws.com/s.cdpn.io/162656/${weather[0].icon}.svg`;
-      const icon2 = `https://openweathermap.org/img/wn/${weather[0].icon}@2x.png`;
+      // const icon = `https://s3-us-west-2.amazonaws.com/s.cdpn.io/162656/${weather[0].icon}.svg`;
+      const icon = `https://openweathermap.org/img/wn/${weather[0].icon}@2x.png`;
       const li = document.createElement("li");
       li.classList.add("city");
       const markup = `
@@ -25,7 +25,7 @@ form.addEventListener("submit", (e) => {
             </h2>
             <div class='city-temp'>${Math.round(main.temp)}</div>
             <figure>
-                <img class='city-icon' src=${icon2} alt ='city' >
+                <img class='city-icon' src=${icon} alt ='city' >
                 <figurecaption>${weather[0].description}</figurecaption>
             </figure>
             </h2>
